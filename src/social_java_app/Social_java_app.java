4a5d8da97f00e9ad97983384b6c041d5fc660bea
Yousef Fekry Farag <yofarag@transit.com.eg>
2025-01-12 13:24:06 +0200

    mvc arch

--- src/social_java_app/Social_java_app.java
+++ src/social_java_app/Social_java_app.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package social_java_app;
 
 import javafx.application.Application;
@@ -13,10 +9,6 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author yofarag
- */
 public class Social_java_app extends Application {
     
     @Override
